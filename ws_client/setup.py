73a389e22bab0531854f6baf182c18eb7e85a6cb
Mariusz Kryński <mrk@sed.pl>
2020-07-04 01:07:57 +0200

    fix path

--- ws_client/setup.py
+++ ws_client/setup.py
@@ -6,7 +6,7 @@ import os
 cur_dir = os.path.dirname(__file__)
 
 shutil.copy(
-    f"{cur_dir}/../custom_component/blitzortung/version.py",
+    f"{cur_dir}/../custom_components/blitzortung/version.py",
     f"{cur_dir}/ws_client/component_version.py",
 )
 setuptools.setup()
