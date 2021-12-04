b462f4c79757c1ac0e81674740990e56963e19bb
Heikki Henriksen <heikkih@gmail.com>
2021-12-04 19:22:43 +0100

    Fixes #34
    Replace deprecated Entity.device_state_attributes with Entity.extra_state_attributes

--- custom_components/blitzortung/sensor.py
+++ custom_components/blitzortung/sensor.py
@@ -99,7 +99,7 @@ class BlitzortungSensor(Entity):
         return self._state
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the state attributes."""
         return self._attrs
 
