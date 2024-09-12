742b87c05e8524d8222eee7497d27038b4457deb
Rogier van Staveren <rogier@batoid.com>
2024-09-12 19:11:58 +0200

    Improve unload entry

--- custom_components/blitzortung/__init__.py
+++ custom_components/blitzortung/__init__.py
@@ -105,19 +105,15 @@ async def async_update_options(hass, config_entry):
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     """Unload a config entry."""
-    coordinator = hass.data[DOMAIN].pop(config_entry.entry_id)
+    coordinator: BlitzortungCoordinator = hass.data[DOMAIN][config_entry.entry_id]
     await coordinator.disconnect()
-    _LOGGER.info("disconnected")
+    _LOGGER.debug("Disconnected")
 
-    # cleanup platforms
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(config_entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
+    # Cleanup platforms.
+    if unload_ok := await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS):
+        # Pop coordinator only when unload was succesfull.
+        hass.data[DOMAIN].pop(config_entry.entry_id)
+
     return unload_ok
 
 
