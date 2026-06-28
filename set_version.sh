fe8f0d13dfb5841ee4e4e4a6eacf52a0424e0152
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-06-28 16:52:01 +0200

    Add Release Drafter to CI (#290)
    * Use release drafter
    
    * Delete set_version.sh script
    
    * Upate labels
    
    * Fix tests
    
    * Missing label
    
    * Checkout is not needed

--- set_version.sh
+++
@@ -1,6 +0,0 @@
-MANIFEST_FILE="custom_components/blitzortung/manifest.json"
-TMPFILE=$(mktemp /tmp/blitzortung-manifest.XXXXXX)
-cp $MANIFEST_FILE $TMPFILE
-jq '.version="'$1'"' < $TMPFILE > $MANIFEST_FILE
-sed -i 's/^__version__ = .*/__version__ = "'"$1"'"/' custom_components/blitzortung/version.py
-rm $TMPFILE
